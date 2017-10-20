c860166f13e0ddbbd2a245db877fdd567b27e673
geeannlopez <geeannmlopez@gmail.com>
2017-10-20 10:27:08 +0800

    home

--- application/views/main/home.php
+++ application/views/main/home.php
@@ -1,3 +1,22 @@
+<style>
+img{
+  width: auto;
+  max-width: 500px;
+  height: auto;
+  max-height: 500px;
+      vertical-align: middle;
+}
+
+img.principals{
+  width: auto;
+  max-width: 100px;
+  height: auto;
+  max-height: 100px;
+      vertical-align: middle;
+}
+</style>
+
+
 <section>
     <!-- *** HOMEPAGE CAROUSEL ***
 _________________________________________________________ -->
@@ -12,14 +31,14 @@ _________________________________________________________ -->
                     <div class="row">
                         <div class="col-sm-5 right">
                             <p>
-                                <img src="<?=base_url()?>/assets/img/logo-aces.png" alt="">
+                                <img src="<?=base_url()?>assets/img/logo-aces.png" alt="">
                             </p>
                             <h1>Multipurpose responsive theme</h1>
                             <p>Business. Corporate. Agency.
                                 <br />Portfolio. Blog. E-commerce.</p>
                         </div>
                         <div class="col-sm-7">
-                            <img class="img-responsive" src="<?=base_url()?>/assets/img/template-homepage.png" alt="">
+                            <img class="img-responsive" src="<?=base_url()?>images/1.jpg" alt="">
                         </div>
                     </div>
                 </div>
@@ -27,7 +46,7 @@ _________________________________________________________ -->
                     <div class="row">
 
                         <div class="col-sm-7 text-center">
-                            <img class="img-responsive" src="<?=base_url()?>/assets/img/template-mac.png" alt="">
+                            <img class="img-responsive" src="<?=base_url()?>images/2.jpg" alt="">
                         </div>
 
                         <div class="col-sm-5">
@@ -54,14 +73,14 @@ _________________________________________________________ -->
                             </ul>
                         </div>
                         <div class="col-sm-7">
-                            <img class="img-responsive" src="<?=base_url()?>/assets/img/template-easy-customize.png" alt="">
+                            <img class="img-responsive" src="<?=base_url()?>images/3.png" alt="">
                         </div>
                     </div>
                 </div>
                 <div class="item">
                     <div class="row">
                         <div class="col-sm-7">
-                            <img class="img-responsive" src="<?=base_url()?>/assets/img/template-easy-code.png" alt="">
+                            <img class="img-responsive" src="<?=base_url()?>images/4.jpg" alt="">
                         </div>
                         <div class="col-sm-5">
                             <h1>Easy to customize</h1>
@@ -79,195 +98,26 @@ _________________________________________________________ -->
 
     <!-- *** HOMEPAGE CAROUSEL END *** -->
 </section>
-<section class="bar background-gray no-mb padding-big text-center-sm">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h2 class="text-uppercase">Everything you need</h2>
-                <p class="lead mb-small">40+ prepared HTML templates</p>
-                <p class="mb-small">We have prepared literally <strong>everything you would possibly need building corporate, e-commerce or portfolio website</strong>. If you still miss something, let us know and we will try to include it in theme's future updates.</p>
 
-                <p><a href="#" class="btn btn-template-main">Read more</a>
-                </p>
-            </div>
-            <div class="col-md-6 text-center">
-                <img src="<?=base_url()?>/assets/img/template-easy-customize.png" alt="" class="img-responsive">
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="bar no-mb color-white padding-big text-center-sm">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 text-center">
-                <img src="<?=base_url()?>/assets/img/template-easy-code.png" alt="" class="img-responsive">
-            </div>
-            <div class="col-md-6">
-                <h2 class="text-uppercase">Clean, valid and documented code &amp; support</h2>
-                <p class="lead mb-small">We made our best to help you</p>
-                <p class="mb-small">Everything in this template is properly named and categorized. In case you don't find in the documentation answer to your question, our support is here to help!</p>
-
-                <p><a href="#" class="btn btn-template-transparent-primary">Read more</a>
-                </p>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="bar background-gray no-mb padding-big text-center-sm">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h2 class="text-uppercase">Easy to customize</h2>
-                <p class="mb-small">We have made this template with focus on future client customization. You get <strong>40+ prepared HTML templates</strong>, many <strong>reusable components</strong> and <strong>LESS files</strong> for even easier template customization.</p>
-
-                <p><a href="#" class="btn btn-template-main">Read more</a>
-                </p>
-            </div>
-            <div class="col-md-6 text-center">
-                <img src="img/template-easy-customize.png" alt="" class="img-responsive">
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="bar background-white no-mb" id="packages">
-    <div class="container" data-animate="fadeInUp">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="heading text-center">
-                    <h2>Packages we offer</h2>
-                </div>
-
-                <p class="lead">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-
-                <div class="row packages">
-
-                    <div class="col-md-3">
-                        <div class="package ">
-                            <div class="package-header light-gray">
-                                <h5>Basic</h5>
-                            </div>
-                            <div class="price">
-                                <div class="price-container">
-                                    <h4><span class="currency">$</span>7</h4>
-                                    <span class="period">
-
-                                        / month
-
-                                    </span>
-                                </div>
-                            </div>
-                            <ul>
-                                <li><i class="fa fa-check"></i>100GB Storage</li>
-                                <li><i class="fa fa-check"></i>Own config file</li>
-                                <li><i class="fa fa-times"></i>Sharing Tools</li>
-                                <li><i class="fa fa-times"></i>Auto Backup</li>
-                                <li><i class="fa fa-times"></i>Extended Security</li>
-                            </ul>
-                            <a href="#" class="btn btn-template-main">
-
-                                Sign Up </a>
-                        </div>
-                    </div>
-                    <!-- / END FIRST PACKAGE -->
-
-                    <div class="col-md-3">
-                        <div class="package ">
-                            <div class="package-header light-gray">
-                                <h5>Standard</h5>
-                            </div>
-                            <div class="price">
-                                <div class="price-container">
-                                    <h4><span class="currency">$</span>21 </h4>
-                                    <span class="period">/ month</span>
-                                </div>
-                            </div>
-                            <ul>
-                                <li><i class="fa fa-check"></i>100GB Storage</li>
-                                <li><i class="fa fa-check"></i>Own config file</li>
-                                <li><i class="fa fa-times"></i>Sharing Tools</li>
-                                <li><i class="fa fa-times"></i>Auto Backup</li>
-                                <li><i class="fa fa-times"></i>Extended Security</li>
-                            </ul>
-                            <a href="#" class="btn btn-template-main">
-
-                                Sign Up </a>
-                        </div>
-                    </div>
-
-                    <div class="col-md-3">
-                        <div class="best-value">
-                            <div class="package">
-                                <div class="package-header">
-                                    <h5>Premium</h5>
-                                    <div class="meta-text">
-                                        Best Value
-                                    </div>
-                                </div>
-                                <div class="price">
-                                    <h4><span class="currency">$</span>53</h4>
-                                    <span class="period">/ month</span>
-                                </div>
-                                <ul>
-                                    <li><i class="fa fa-check"></i>100GB Storage</li>
-                                    <li><i class="fa fa-check"></i>Own config file</li>
-                                    <li><i class="fa fa-times"></i>Sharing Tools</li>
-                                    <li><i class="fa fa-times"></i>Auto Backup</li>
-                                    <li><i class="fa fa-times"></i>Extended Security</li>
-                                </ul>
-                                <a href="#" class="btn btn-template-main">
-
-                                    Sign Up </a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-3">
-                        <div class="package">
-                            <div class="package-header light-gray">
-                                <h5>Ultimate</h5>
-                            </div>
-                            <div class="price">
-                                <h4><span class="currency">$</span>99 </h4>
-                                <span class="period">/ month</span>
-                            </div>
-                            <ul>
-                                <li><i class="fa fa-check"></i>100GB Storage</li>
-                                <li><i class="fa fa-check"></i>Own config file</li>
-                                <li><i class="fa fa-times"></i>Sharing Tools</li>
-                                <li><i class="fa fa-times"></i>Auto Backup</li>
-                                <li><i class="fa fa-times"></i>Extended Security</li>
-                            </ul>
-                            <a href="#" class="btn btn-template-main">
-
-                                Sign Up </a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="bar background-gray no-mb">
+<section class="bar background-white no-mb">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
+                        <div class="heading text-center">
+                            <h2>Our Principals</h2>
+                        </div>
                 <ul class="owl-carousel customers">
                     <li class="item">
-                        <img src="<?=base_url()?>/assets/img/customer-1.png" alt="" class="img-responsive">
+                        <img src="<?=base_url()?>images/winters.jpeg" alt="" class="img-responsive principals">
                     </li>
                     <li class="item">
-                        <img src="<?=base_url()?>/assets/img/customer-2.png" alt="" class="img-responsive">
+                        <img src="<?=base_url()?>images/marsh.jpg" alt="" class="img-responsive principals">
                     </li>
                     <li class="item">
-                        <img src="<?=base_url()?>/assets/img/customer-3.png" alt="" class="img-responsive">
+                        <img src="<?=base_url()?>images/suco.jpg" alt="" class="img-responsive principals">
                     </li>
                     <li class="item">
-                        <img src="<?=base_url()?>/assets/img/customer-4.png" alt="" class="img-responsive">
+                        <img src="<?=base_url()?>images/esi.jpg" alt="" class="img-responsive principals">
                     </li>
                     <li class="item">
                         <img src="<?=base_url()?>/assets/img/customer-5.png" alt="" class="img-responsive">
@@ -283,37 +133,4 @@ _________________________________________________________ -->
     </div>
 </section>
 
-<section class="bar background-image-fixed-2 no-mb color-white text-center">
-    <div class="dark-mask"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="icon icon-lg"><i class="fa fa-file-code-o"></i>
-                </div>
-                <h1>Do you want to see more?</h1>
-                <p class="lead">We have prepared for you more than 40 different HTML pages, including 5 variations of homepage.</p>
-                <p class="text-center">
-                    <a href="index3.html" class="btn btn-template-transparent-black btn-lg">Check other homepage</a>
-                </p>
-            </div>
 
-        </div>
-    </div>
-</section>
-
-<!-- *** GET IT ***
-_________________________________________________________ -->
-
-<div id="get-it">
-    <div class="container">
-        <div class="col-md-8 col-sm-12">
-            <h3>Do you want cool website like this one?</h3>
-        </div>
-        <div class="col-md-4 col-sm-12">
-            <a href="#" class="btn btn-template-transparent-primary">Buy this template now</a>
-        </div>
-    </div>
-</div>
-
-
-<!-- *** GET IT END *** -->
