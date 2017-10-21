413d18451ba04b685059f1c88977f2e4bcb569f7
Cyrus Aaron <cyruuusaaron@gmail.com>
2017-10-21 13:29:36 +0800

    super duper minor frontend changes on home page

--- application/views/main/home.php
+++ application/views/main/home.php
@@ -307,10 +307,10 @@ _________________________________________________________ -->
 <div id="get-it">
     <div class="container">
         <div class="col-md-8 col-sm-12">
-            <h3>Do you want cool website like this one?</h3>
+            <h3>WE GIVE YOU ONLY THE BEST</h3>
         </div>
         <div class="col-md-4 col-sm-12">
-            <a href="#" class="btn btn-template-transparent-primary">Buy this template now</a>
+            <a href="#" class="btn btn-template-transparent-primary">SIGN UP NOW</a>
         </div>
     </div>
 </div>
